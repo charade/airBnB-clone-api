549e2d08fd839ba49bd80f16bb120ec078a0e25f
puthpiseth <puthpiseth_tun@yahoo.com>
2021-05-02 01:04:02 +0200

    Refacto eslint config

--- src/controller/hostController.js
+++ src/controller/hostController.js
@@ -2,82 +2,74 @@ const { response } = require('express');
 const host = require('../model/hostModel');
 const utils = require('../model/modelUtils');
 
-
 // Add a place
-exports.add_a_place = async(req, res) => {
-    
-    const {user_id, city_name} = req.params;
-    ///before adding a place we check if it exists
-    const city = await utils.get_a_city_byName(city_name);
-    console.log(city[0])
-    if(city[0].length !== 0){
-        const city_id = city[0][0].id;
-        await host.add_a_place(user_id,req.body, city_id);
-        res.status(200).json({message: "successfully added a place!"});
-        return;
-    }
-    res.status(400).json({message: "city not found!"})
-}
+exports.add_a_place = async (req, res) => {
+  const { user_id, city_name } = req.params;
+  /// before adding a place we check if it exists
+  const city = await utils.get_a_city_byName(city_name);
 
+  if (city[0].length !== 0) {
+    const city_id = city[0][0].id;
+    await host.add_a_place(user_id, req.body, city_id);
+    res.status(200).json({ message: 'successfully added a place!' });
+    return;
+  }
+  res.status(400).json({ message: 'city not found!' });
+};
 
 // Delete a place
-exports.delete_a_place = (req, res)=>{
-    const {place_id, user_id} = req.params;
-    try{
-        host.delete_a_place(place_id, user_id);
-        res.status(200).json({ message: 'succesfully deleted' });
-    }
-    catch(err){
-        res.status(500).json({err: err});
-    }
-}
+exports.delete_a_place = (req, res) => {
+  const { place_id, user_id } = req.params;
+  try {
+    host.delete_a_place(place_id, user_id);
+    res.status(200).json({ message: 'succesfully deleted' });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+};
 
+// Get host's places list
+exports.get_host_places_list = async (req, res) => {
+  const { user_id } = req.params;
+  try {
+    const places = await host.get_places_in_rent_list(user_id);
 
-//Get host's places list
-exports.get_host_places_list = async(req, res)=>{
-    const {user_id} = req.params;
-    try{
-        const places = await host.get_places_in_rent_list(user_id);
-       
-        if(places[0][0].length !== 0){
-            res.status(200).json({message: "success!", response: response});
-            return;
-        }
-        res.status(400).json({message: "not found!"})
+    if (places[0][0].length !== 0) {
+      res.status(200).json({ message: 'success!', response });
+      return;
     }
-    catch(err){
-        res.status(500).json({message: "connection failed!"})
-    }
-}
+    res.status(400).json({ message: 'not found!' });
+  } catch (err) {
+    res.status(500).json({ message: 'connection failed!' });
+  }
+};
 
 // Host can change a place's info
-exports.modify_place_info = async (req, res)=>{
-    const {column, place_id} = req.params;
-    const {newValue} = req.body;
-    try{
-        await host.editInfo(column, newValue, place_id)
-        res.status(200).json({message: 'data edited'})
-    }
-    catch(err){
-        res.status(500).json({message: err})
-    }
-}
+exports.modify_place_info = async (req, res) => {
+  const { column, place_id } = req.params;
+  const { newValue } = req.body;
+  try {
+    await host.editInfo(column, newValue, place_id);
+    res.status(200).json({ message: 'data edited' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
 
 // All places booked
-exports.all_host_booked_places = async(req, res)=>{
-    const{user_id} = req.params;
-    try{
-        const rentBookedPlaces = await host.all_booked_places(user_id);
-        console.log(rentBookedPlaces)
-        if(rentBookedPlaces[0][0].length === 0){
-            res.status(400).json({message: "no places booked"});
-            return;
-        }
-        const bookedPlaces = [...rentBookedPlaces[0]]
-        res.status(200).json({message: "success!", response: bookedPlaces});
-    }
-    catch(err){
-        res.status(500).json({message: "connection failed!"})
-    }
-}
+exports.all_host_booked_places = async (req, res) => {
+  const { user_id } = req.params;
+  try {
+    const rentBookedPlaces = await host.all_booked_places(user_id);
 
+    if (rentBookedPlaces[0][0].length === 0) {
+      res.status(400).json({ message: 'no places booked' });
+      return;
+    }
+
+    const bookedPlaces = [...rentBookedPlaces[0]];
+    res.status(200).json({ message: 'success!', response: bookedPlaces });
+  } catch (err) {
+    res.status(500).json({ message: 'connection failed!' });
+  }
+};
--- src/controller/touristController.js
+++ src/controller/touristController.js
@@ -1,81 +1,73 @@
-const { response } = require('express');
 const touristModel = require('../model/touristModel');
-const modelUtils = require('../model/modelUtils')
+const modelUtils = require('../model/modelUtils');
 
-exports.add_place_to_bookmarks = async (request, response) =>{
+exports.add_place_to_bookmarks = async (request, response) => {
+  const { user_id, place_id } = request.params;
+  const { check_in, check_out } = request.query;
+  const date = { in: check_in, out: check_out };
 
-    const { user_id, place_id } = request.params;
-    const { check_in, check_out } = request.query;
-    const date = { in : check_in, out: check_out };
+  try {
+    // format ISO date to mysql TIMESTAMP
+    date.in = new Date(date.in).toISOString().replace('T', ' ').replace('Z', '')
+      .slice(0, 19);
+    date.out = new Date(date.out).toISOString().replace('T', ' ').replace('Z', '')
+      .slice(0, 19);
+    await touristModel.book_a_place(user_id, place_id, date);
+    response.status(200).json({ message: 'booked' });
+  } catch (err) {
+    response.status(500).json({ err: `can't connect${err}` });
+  }
+};
 
-    try{
-        //format ISO date to mysql TIMESTAMP
-        date.in = new Date(date.in).toISOString().replace('T', ' ').replace('Z', "").slice(0,19);
-        date.out = new Date(date.out).toISOString().replace('T', ' ').replace('Z', "").slice(0,19);
-        await touristModel.book_a_place(user_id, place_id, date);
-        response.status(200).json({message : "booked"});
+// cancel from bookmarks
+exports.cancel_booked_place = async (req, res) => {
+  const { user_id, booking_id } = req.params;
+
+  try {
+    await touristModel.cancelBooking(user_id, booking_id);
+    res.status(200).json({ message: 'succesfully deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+// all place booked by tourist
+
+exports.all_places_booked = async (req, res) => {
+  const { user_id } = req.params;
+
+  try {
+    const response = await touristModel.get_all_booked_places(user_id);
+    res.status(200).json({ response: response[0] });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+};
+
+exports.search_places_byDate = async (req, res) => {
+  const { date_in, date_out } = req.query;
+  const date = `${date_in} / ${date_out}`;
+  try {
+    const response = await touristModel.get_all_places();
+    const array = response[0].filter((el) => el.available === date);
+    res.status(200).json({ response: array });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+exports.search_places_byCity = async (req, res) => {
+  const { city_name } = req.query;
+  try {
+    const getCityArr = await modelUtils.get_a_city_byName(city_name);
+
+    if (getCityArr[0].length !== 0) {
+      const city_id = getCityArr[0][0].id;
+      const placesArr = await modelUtils.all_places_in_the_city(city_id);
+      res.status(200).json({ response: placesArr[0] });
+      return;
     }
-    catch(err){
-        response.status(500).json({ err : "can't connect" + err })
-    }
-}
-
-//cancel from bookmarks
-exports.cancel_booked_place = async (req, res)=>{
-    const {user_id, booking_id} = req.params;
-   console.log(user_id, booking_id);
-    try{
-
-        await touristModel.cancelBooking(user_id, booking_id);
-        res.status(200).json({ message: 'succesfully deleted' });
-    }
-    catch(err){
-        res.status(500).json({ error : err });
-    }
-}
-//all place booked by tourist
-
-exports.all_places_booked = async (req, res)=>{
-    const {user_id} = req.params;
-    
-    try{
-        const response = await touristModel.get_all_booked_places(user_id);
-        res.status(200).json({ response : response[0] });
-    }
-    catch(err){
-        console.error(err);
-    }
-}
-
-exports.search_places_byDate = async (req, res) =>{
-    const {date_in, date_out} = req.query;
-    const date = date_in + " / " + date_out;
-    try{
-        const response = await touristModel.get_all_places();
-        const array = response[0].filter(el=> el.available === date);
-        res.status(200).json({response:array})
-    }
-    catch(err){
-        res.status(500).json({error:err});
-    }
-}
-
-exports.search_places_byCity = async (req, res) =>{
-    const {city_name} = req.query;
-    try{   
-        const getCityArr = await modelUtils.get_a_city_byName(city_name); 
-
-        if(getCityArr[0].length !== 0){
-            const city_id = getCityArr[0][0].id;
-            const placesArr = await modelUtils.all_places_in_the_city(city_id);
-            res.status(200).json({response:placesArr[0]})
-            return;
-        }
-        res.status(400).json({message: "city not found!"})
-    }
-    catch(err){
-        res.status(500).json({error:err});
-    }
-}
-
-
+    res.status(400).json({ message: 'city not found!' });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
